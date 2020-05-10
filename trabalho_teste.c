7ac043fab930c46232fb7a0bc79cfe016d3fb920
leoperassoli <60322544+leoperassoli@users.noreply.github.com>
2020-05-09 21:58:50 -0300

    Update trabalho_teste.c

--- trabalho_teste.c
+++ trabalho_teste.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+// Province/State,Country/Region,Lat,Long,Date,Confirmed,Deaths,Recovered
+#define TAMs 100
+#define TAMc 100
+
+typedef dado{
+	char State[TAMs], Country[TAMc], Date[10];
+	float lat, longe;
+	int conf,death,recov;
+}Dado;
+
+
+
+
 
 int main(){
+	
+   	char c[350];
+   	int i,j;
+	Dado exemplo;	
+
+	
 	
 	FILE *fptr;
 	fptr = fopen("covid_19_clean_complete.csv","r");
@@ -10,11 +31,17 @@ int main(){
        return 1;
    }
    
-   	char c[350];
-   	
 	fscanf(fptr, "%s", c);
-   	printf("%s", c);
+	for(i = 0; i<3; i++){
+		fscanf(fptr, "%s", c);
+		printf("%s \n", c);
+	}
 	
+	strtok(char *str, const char *delim)
+	
+	for(i = 0; i<3; i++){
+   		printf("%c", c[i])	
+	}
 	
 	
 	return 0;
